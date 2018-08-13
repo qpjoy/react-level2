b83ce094b0ce564939b1b55d2b25632be1f0b351
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-13 18:18:16 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Toggle from './ToggleRPC';
+import Portal from './Portal';
+import Modal from './Modal';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Modal />
+        <Toggle>
+          {({ on, toggle }) => (
+            <Fragment>
+              <button onClick={toggle}>Login</button>
+              <Modal on={on} toggle={toggle}>
+                <h1>Child Element</h1>
+              </Modal>
+            </Fragment>
+          )}
+        </Toggle>
+        <Portal>
+          <h1>Test1</h1>
+        </Portal>
+      </div>
+    );
+  }
+}
+
+export default App;
