966fd3cb19258aa1352f7134b4da3b2fd5008cb1
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-14 11:15:43 -0500

    import and export transition function to Utilities index

--- src/Utilities/index.js
+++ src/Utilities/index.js
@@ -1,5 +1,6 @@
 import Portal from './Portal';
 import Toggle from './Toggle';
 import elevation from './elevation';
+import transition from './transition';
 
-export { Portal, Toggle, elevation };
+export { Portal, Toggle, elevation, transition };
