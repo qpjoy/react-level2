6bb0c0a945d23be238f273025b219eb6307c4b3c
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-14 12:50:30 -0500

    import and export center component to Utilities index

--- src/Utilities/index.js
+++ src/Utilities/index.js
@@ -3,6 +3,7 @@ import Toggle from './Toggle';
 import elevation from './elevation';
 import transition from './transition';
 import colors from './colors';
+import center from './center';
 
 // colors can be imported as: import { colors} from 'Utilities';
 // from there it is used/accessed via color: ${colors.black};
@@ -13,4 +14,4 @@ import colors from './colors';
 export * from './colors';
 export * from './position';
 
-export { Portal, Toggle, elevation, transition, colors };
+export { Portal, Toggle, elevation, transition, colors, center };
