fcb4652ed081c4705d7449d0d7fd633e74efc55a
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-14 11:22:05 -0500

    set transition

--- src/Elements/Cards.js
+++ src/Elements/Cards.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
-import { elevation } from 'Utilities';
+import { elevation, transition } from 'Utilities';
 
 export const Card = styled.div`
   background: white;
   border-radius: 5px;
-  ${elevation[4]};
   padding: 15px;
+  ${elevation[4]};
+  ${transition()};
+  :hover {
+    ${elevation[5]};
+  }
 `;
