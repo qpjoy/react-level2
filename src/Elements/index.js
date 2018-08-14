7fec98633f2cfd82287d763335acff0f1c64287e
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-13 20:01:51 -0500

    add comment

--- src/Elements/index.js
+++ src/Elements/index.js
@@ -2,5 +2,6 @@ import Icon from './Icon';
 import Modal from './Modal';
 
 // Imports every element from Cards, then exports them--all in one line;
+// non-default exports can be imported using this export * from syntax
 export * from './Cards';
 export { Icon, Modal };
