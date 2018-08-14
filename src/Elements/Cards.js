4351f7cbf3188eaa11ca03e4f61c9842d73b01fd
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-14 11:49:57 -0500

    set color using colors library

--- src/Elements/Cards.js
+++ src/Elements/Cards.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
-import { elevation, transition } from 'Utilities';
+import { elevation, transition, colors, black } from 'Utilities';
 
 export const Card = styled.div`
   background: white;
   border-radius: 5px;
+  // color: ${colors.teal};
+  color: ${black};
   padding: 15px;
   ${elevation[4]};
   ${transition({})};
