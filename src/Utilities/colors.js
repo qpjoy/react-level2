56adaf0188d3e1a735a4b3c3212efdd9831b6e1e
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-14 11:54:16 -0500

    add comments

--- src/Utilities/colors.js
+++ src/Utilities/colors.js
@@ -4,6 +4,12 @@ export const black = '#222';
 export const teal = '#82d8d8';
 export const purple = '#524767';
 
+// colors can be imported as: import { colors} from 'Utilities';
+// from there it is used/accessed via color: ${colors.black};
+// Alternatively, a specific color may be imported and accessed with the
+// following syntax: import { black } from 'Utilities'; color: ${black};
+// This is because we have a global and individual export from './colors';
+
 export default {
   lightGrey,
   grey,
--- src/Utilities/index.js
+++ src/Utilities/index.js
@@ -4,6 +4,12 @@ import elevation from './elevation';
 import transition from './transition';
 import colors from './colors';
 
+// colors can be imported as: import { colors} from 'Utilities';
+// from there it is used/accessed via color: ${colors.black};
+// Alternatively, a specific color may be imported and accessed with the
+// following syntax: import { black } from 'Utilities'; color: ${black};
+// This is because we have a global and individual export from './colors';
+
 export * from './colors';
 
 export { Portal, Toggle, elevation, transition, colors };
