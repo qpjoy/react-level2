ce7c129362ead8832cc155547f355ae0ed1a2da8
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-14 13:25:33 -0500

    create User component

---
+++ src/User.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class User extends Component {
+  render() {
+    return (
+      <div>
+        <h1>User Info</h1>
+      </div>
+    );
+  }
+}
